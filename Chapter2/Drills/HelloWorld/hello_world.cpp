8aeedae5245679a12d7f3f0c94389c75bdd89c06
NightlyDev <Alexander.Hadula@gmail.com>
2021-03-27 01:23:50 +0100

    test: another git test

--- Chapter2/Drills/HelloWorld/hello_world.cpp
+++ Chapter2/Drills/HelloWorld/hello_world.cpp
@@ -1,5 +1,5 @@
 #include "../../../std_lib_facilities.h"
 int main() {    // C++ programs start by executing the function main
     cout << "Hello, World!\n";  // output "Hello, World!"
-    return 0; //git test
+    return 0; //git
 }
