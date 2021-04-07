20d76b5153c36e4e337ad6779b1cb56021767a26
NightlyDev <Alexander.Hadula@gmail.com>
2021-04-07 22:18:23 +0200

    feat: changed code according to 1.

--- Chapter3/drills/form_letter.cpp
+++ Chapter3/drills/form_letter.cpp
@@ -2,8 +2,8 @@
 #include "../../std_lib_facilities.h"
 
 int main() {
-    cout << "Please enter your first name (followed by 'enter'):\n";
+    cout << "Enter the name of the person you want to write to (followed by 'enter'):\n";
     string first_name = "";
     cin >> first_name;
-    cout << "Hello, " << first_name << "!\n";
+    cout << "Dear " << first_name << ",\n";
 }
