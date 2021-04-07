e29bfe1339f2984178dcac880ad6154c5ab01369
NightlyDev <Alexander.Hadula@gmail.com>
2021-04-07 03:52:43 +0200

    add: cruel world try-this assignment

---
+++ Chapter3/try-this/cruel_world.cpp
@@ -0,0 +1,6 @@
+#include "../../std_lib_facilities.h"
+
+int main() {    // fixed by writing Main() to main()
+    string s = "Goodbye, curel world! ";    // fixed by writing STRING to string
+    cout << s << '\n';  //fixed by writing cOut to cout and S to s
+}
