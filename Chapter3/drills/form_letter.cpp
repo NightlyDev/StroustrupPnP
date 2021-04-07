98969e756ce85bba8c9838a5c58d9364cfc28be8
NightlyDev <Alexander.Hadula@gmail.com>
2021-04-07 22:22:04 +0200

    feat: changed code according to 2.

--- Chapter3/drills/form_letter.cpp
+++ Chapter3/drills/form_letter.cpp
@@ -6,4 +6,6 @@ int main() {
     string first_name = "";
     cin >> first_name;
     cout << "Dear " << first_name << ",\n";
+    cout << "How are you? I am fine. I miss meeting my friends at the uni campus because there is a weird virus going around right now. \n";
+    cout << "I hope that you will be fine during this pandemic, considering that we don't know what repurcussions this virus has once you've been sick. \n"
 }
